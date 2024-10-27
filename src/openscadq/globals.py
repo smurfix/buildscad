Fix arbitrary rotation

src/openscadq/globals.py
@@ -260,17 +260,28 @@ class _Mods(DynEnv):
         if ch is None:
             return None
         if v is not None and v != [0, 0, 0]:
-            return ch.rotate(v, a)
+            if v == [1,0,0]:
+                return ch.rotate(Axis.X,a)
+            if v == [0,1,0]:
+                return ch.rotate(Axis.Y,a)
+            if v == [0,0,1]:
+                return ch.rotate(Axis.Z,a)
+
+            # now things get difficult
+            from scipy.spatial.transform import Rotation
+            vl = math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2])
+            r = Rotation.from_rotvec(tuple(x/vl*a for x in v), degrees=True)
+            a = r.as_euler("xyz", degrees=True)
         elif isinstance(a, (float, int)):
             return ch.rotate(Axis.Z, a)
-        else:
-            if a[0]:
-                ch = ch.rotate(Axis.X, a[0])
-            if a[1]:
-                ch = ch.rotate(Axis.Y, a[1])
-            if a[2]:
-                ch = ch.rotate(Axis.Z, a[2])
-            return ch
+
+        if a[0]:
+            ch = ch.rotate(Axis.X, a[0])
+        if a[1]:
+            ch = ch.rotate(Axis.Y, a[1])
+        if a[2]:
+            ch = ch.rotate(Axis.Z, a[2])
+        return ch
 
     def difference(self) -> Shape:  # noqa:D102
         ch = iter(self.children())